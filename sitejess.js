72e8c7e5fd5690b498ae40b6fb5999ee41034f03
Ana-Jessica <130403589+Ana-Jessica@users.noreply.github.com>
2023-04-10 17:27:40 -0300

    Add files via upload

---
+++ sitejess.js
@@ -0,0 +1,7 @@
+// var idade = prompt('Digite sua idade:');
+
+// if (idade < 18) {
+//     alert('Melhor começar a aprender programação desde cedo!');
+// } else {
+//     alert('Por que não começar agora a aprender programação?');
+// }
